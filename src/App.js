4be539cab7a0bff5f9f6f30e560cbcefbeff7ae0
David <david@davids-mbp.lan>
2019-11-17 17:18:30 +0000

    update css page and loading with react

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React from 'react';
+
+import React, {Component} from 'react';
+
+import EpicMenu from './EpicMenu';
 
 
-function App() {
+class App extends Component {
+
+  render() {
   return (
    
 
-    <div className =" container center">
-
-    <nav className = "manu">
-       <h1 className=" menu_logo">Epic co.</h1>
-
-       <div className = "manu-right">
-        <ul className = " menu_list">
-        <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
-        <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
-        <li className="menu__list-item"><a className="menu__link" href="#">Portfolio</a></li>
-        <li className="menu__list-item"><a className="menu__link" href="#">Contact</a></li>
-
-
-        </ul>
-        <button className="menu__search-button"></button>
-        
-        <form className="menu__search-form hide" method="POST">
-        <input class="menu__search-input" placeholder="Type and hit enter"/>
-
-        </form>
-       
-       </div>
-      
-    </nav>
+    <div className ="container  center">
+    
+    <EpicMenu />
+  
 
 
 
@@ -43,6 +28,7 @@ function App() {
       
   
   );
+}
 }
 
 export default App;
---
+++ src/EpicMenu.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import logo from './logo.png';
+import searchIcon from './search-icon.png';
+
+
+class EpicMenu extends  Component {
+
+    render(){
+
+        return(
+
+            <nav className = 'menu'>
+
+             <h1  className=" menu_logo">DLCC.</h1>
+
+<div className = "menu__right">
+ <ul className = " menu_list">
+ <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
+ <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
+ <li className="menu__list-item"><a className="menu__link" href="#">Portfolio</a></li>
+ <li className="menu__list-item"><a className="menu__link" href="#">Contact</a></li>
+
+
+ </ul>
+ <button style = {{
+   backgroundImage: 'url(' + searchIcon +')'
+ }} className="menu__search-button"></button>
+ 
+ <form className="menu__search-form hide" method="POST">
+ <input className="menu__search-input" placeholder="Type and hit enter"/>
+
+ </form>
+
+</div>
+                
+            </nav>
+        )
+    }
+
+
+
+}
+
+export default EpicMenu;
